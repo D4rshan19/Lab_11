e66f7a71a6d1a2622d60b73fa0717b2f3b14eb69
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-12-03 08:55:11 -0500

    Lab_11

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -1,14 +1,32 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListMaker {
 
     static ArrayList<String> places = new ArrayList <String>();
-    places.add("Germany");
-    places.add("Ireland");
-    places.add("Switzerland");
-
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        //List with 10 items inside it
+        places.add("Germany");
+        places.add("Ireland");
+        places.add("Switzerland");
+        places.add("Greenland");
+        places.add("Greece");
+        places.add("Japan");
+        places.add("Russia");
+        places.add("Indonesia");
     }
+
+
+
+
+
+    //Asks the user for index of value they want to remove from the list
+    private static boolean deleteMethod(int index ) {
+        places.remove(index);
+    }
+
+    //Asks the user for a value that will be added to the list
 }
