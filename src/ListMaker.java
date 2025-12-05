10ff68a218e67e7789a26991cd4dde402f089efd
D4rshan19 <dhimal.621185@swl.k12.oh.us>
2025-12-05 08:49:08 -0500

    Lab_11

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -26,30 +26,29 @@ public class ListMaker {
 
     //Asks the user for index of value they want to remove from the list
     private static void deleteMethod() {
-        int index = InputHelper.getRangedInt(scan, "Enter the position of the item you want to remove", 0,places.size() -1 );
+        int index = InputHelper.getRangedInt(scan, "Enter the position of the item you want to remove [D]", 0,places.size() -1 );
         places.remove(index);
     }
 
     //Asks the user for a value that will be added to the list
     private static void addMethod() {
-        String newIndex = InputHelper.(1 0, 10, "Would you like to add a method? ");
-        places.add(value);
+        String newIndex = scan.nextLine();
+        System.out.println("Did you want to add a value to the list? If so which value? [A] ");
+        places.add(newIndex);
     }
 
+
     //Prints out the current list
-    private static boolean printMethod(int print) {
+    private static void printMethod() {
         for (int i = 0; i < places.size(); i++) {
             System.out.println(places.get(i));
         }
+        System.out.println("Do you want to print the list? [P]");
     }
 
     //Asks the user if they are sure they want to quit
-    private static boolean quitMethod(int quit) {
-        boolean quit = false;
-       quit = InputHelper.getYNConfirm(scan, "Would you like to quit?  [Y/N]");
-       if (quit) {
-           break;
-       }
+    private static void quitMethod() {
+        String quit = InputHelper.getYNConfirm(scan, "Would you like to quit? [Q]");
     }
 }
 
