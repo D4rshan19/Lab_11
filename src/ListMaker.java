e8f8c96f3fac27551e667587b2062fb0c91f9ce0
Darshan Dhimal <darshandhimal83@gmail.com>
2025-12-08 22:58:06 -0500

    Done

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -7,7 +7,10 @@ public class ListMaker {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+
+        //Declare variables
+        String userMenu;
+        boolean stop = false;
 
         //List with 10 items inside it
         places.add("Germany");
@@ -18,32 +21,55 @@ public class ListMaker {
         places.add("Japan");
         places.add("Russia");
         places.add("Indonesia");
+        places.add("Bali");
+        places.add("Maldives");
+
+        //Initial printing of list
+        System.out.println("Here is the current list: ");
+
+
+        //Actual "code", portion of project
+
+        do {
+            userMenu = InputHelper.getRegExString(scan, "Choose an option from the menu: \n A - Add an item to the list \n D - Delete an item to the list \n P - Print the list \n Q - Quit the program", "[AaDdPpQq]");
+
+            if (userMenu.equalsIgnoreCase("A")) {
+                addMethod();
+            } else if (userMenu.equalsIgnoreCase("D")) {
+                deleteMethod();
+            } else if (userMenu.equalsIgnoreCase("P")) {
+                printMethod();
+            } else {
+                 quitMethod();
+            }
+        } while (!stop);
     }
-
-
-
 
 
     //Asks the user for index of value they want to remove from the list
     private static void deleteMethod() {
-        int index = InputHelper.getRangedInt(scan, "Enter the position of the item you want to remove [D]", 0,places.size() -1 );
+        int index = InputHelper.getRangedInt(scan, "Enter the position of the item you want to remove", 0,places.size() -1 );
         places.remove(index);
+        scan.nextLine();
     }
 
     //Asks the user for a value that will be added to the list
     private static void addMethod() {
         String newIndex = scan.nextLine();
-        System.out.println("Did you want to add a value to the list? If so which value? [A] ");
+        System.out.println("Did you want to add a value to the list? If so which value? ");
         places.add(newIndex);
+        scan.nextLine();
     }
 
 
     //Prints out the current list
     private static void printMethod() {
         for (int i = 0; i < places.size(); i++) {
+            System.out.println(i);
+            System.out.println(" - ");
             System.out.println(places.get(i));
+            System.out.println();
         }
-        System.out.println("Do you want to print the list? [P]");
     }
 
     //Asks the user if they are sure they want to quit
